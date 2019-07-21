fecc9d0fa0f24e6f8bd4f624104844720e4ea80d
Felix <felix.xu.mail@gmail.com>
2019-07-21 14:51:25 +0800

    init

---
+++ js/main.js
@@ -0,0 +1,52 @@
+$(window).load(function() {
+    $("#preloader").fadeOut("slow");
+});
+
+$(document).ready(function(){
+
+
+     new WOW().init();
+
+
+     $('#top-nav').onePageNav({
+        currentClass: 'current',
+        changeHash: true,
+        scrollSpeed: 1200
+    });
+
+
+    //animated header class
+    $(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+     //console.log(scroll);
+    if (scroll > 200) {
+        //console.log('a');
+        $(".navigation").addClass("animated");
+    } else {
+        //console.log('a');
+        $(".navigation").removeClass("animated");
+    }});
+
+    $year = $('#timing_dashboard').data('year');
+    $month = $('#timing_dashboard').data('month');
+    $day = $('#timing_dashboard').data('day');
+    $('#timing_dashboard').countUp({
+        targetDate: {
+            'day':      $day,
+            'month':    $month,
+            'year':     $year,
+            'hour':     0,
+            'min':      0,
+            'sec':      0,
+        },
+        omitWeeks: true
+    });
+
+});
+
+
+
+
+
+
+
